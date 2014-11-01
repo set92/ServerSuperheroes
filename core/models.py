ff3ba1aba628784b84158ef5710ce3d1ce15fc5b
Javier Domingo Cansino <javierdo1@gmail.com>
2014-11-01 21:16:00 +0100

    Corrected using abstract class for users and making entity a model

--- core/models.py
+++ core/models.py
@@ -2,9 +2,9 @@ from django.contrib.gis.db import models as gis_models
 from django.db import models
 from django.contrib.auth import models as auth_models
 
-class Entity():
+class Entity(gis_models.Model):
     position = gis_models.PointField()
     objects = gis_models.GeoManager()
 
-class User(auth_models.User, Entity):
+class User(auth_models.AbstractUser, Entity):
     pass
