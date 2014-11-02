5ecfe4be090973fe312dac9ac247e971c1ec989f
Javier Domingo Cansino <javierdo1@gmail.com>
2014-11-02 11:32:58 +0100

    Adding the api to the registered urls

--- ServerSuperheroes/urls.py
+++ ServerSuperheroes/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from core.api import UserResource
+
+user_resource = UserResource()
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(user_resource.urls)),
 )
